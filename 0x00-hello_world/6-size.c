ef2b001dc5ea859e4f23d10283651e7c0323dd5c
gbaingar <gbengatalabi3@gmail.com>
2023-08-10 19:42:09 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,12 +11,10 @@ int t;
 long n;
 long long int g;
 float f;
-
 printf("Size of a char: %zu byte(s)\n", sizeof(c));
 printf("Size of an int: %zu byte(s)\n", sizeof(t));
 printf("Size of a long int: %zu byte(s)\n", sizeof(n));
 printf("Size of a long long int: %zu byte(s)\n", sizeof(g));
-printf("Size of a float: %zu bytes(s)\n", sizeof(f));
-
+printf("Size of a float: %zu byte(s)\n", sizeof(f));
 return (0);
 }
