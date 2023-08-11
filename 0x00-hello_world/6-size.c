4842dc988f20380fc82c76fb64dfdc2e614c0c1d
gbaingar <gbengatalabi3@gmail.com>
2023-08-10 18:59:51 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 /**
- * main - A program that uses the printf function
+ * main - A program that shows the size of various types
  *
  * Return: 0 (Success)
  */
@@ -16,6 +16,7 @@ printf("Size of a char: %zu byte(s)\n", sizeof(c));
 printf("Size of an int: %zu byte(s)\n", sizeof(t));
 printf("Size of a long int: %zu byte(s)\n", sizeof(n));
 printf("Size of a long long int: %zu byte(s)\n", sizeof(g));
-printf("Size of a float: %zu bytes(s)\n", sizeof(f));
+printf("Size of a float: %zu bytes(s)", sizeof(f));
+
 return (0);
 }
